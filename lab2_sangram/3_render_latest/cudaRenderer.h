fdd2d4ef80f43391cdd0a724b945dab113873bed
Sangramsinh Kate <skate@login1.maverick2.tacc.utexas.edu>
2021-03-28 23:02:38 -0500

    adding the latest code for exclusive scan

---
+++ lab2_sangram/3_render_latest/cudaRenderer.h
@@ -0,0 +1,88 @@
+#ifndef __CUDA_RENDERER_H__
+#define __CUDA_RENDERER_H__
+
+#ifndef uint
+#define uint unsigned int
+#endif
+
+#include "circleRenderer.h"
+
+
+class CudaRenderer : public CircleRenderer {
+
+private:
+
+    Image* image;
+    SceneName sceneName;
+
+    int numCircles;
+    float* position;
+    float* velocity;
+    float* color;
+    float* radius;
+
+    float* cudaDevicePosition;
+    float* cudaDeviceVelocity;
+    float* cudaDeviceColor;
+    float* cudaDeviceRedColor;
+    float* cudaDeviceBlueColor;
+    float* cudaDeviceGreenColor;
+    float* cudaDeviceRadius;
+    float* cudaDeviceImageData;
+
+    int* circleBlockArray;
+    int* host_circlePerBlock;
+    int* device_circlePerBlock;
+    int* circleBlockIdx;
+
+public:
+
+    CudaRenderer();
+    virtual ~CudaRenderer();
+
+    const Image* getImage();
+
+    void setup();
+
+    void loadScene(SceneName name);
+
+    void allocOutputImage(int width, int height);
+
+    void clearImage();
+
+    void advanceAnimation();
+
+    void render();
+
+    void shadeRedPixel(
+        int circleIndex,
+        float pixelCenterX, float pixelCenterY,
+        float px, float py, float pz,
+        float* pixelData);
+
+    void shadeBluePixel(
+        int circleIndex,
+        float pixelCenterX, float pixelCenterY,
+        float px, float py, float pz,
+        float* pixelData);
+    void shadeGreenPixel(
+        int circleIndex,
+        float pixelCenterX, float pixelCenterY,
+        float px, float py, float pz,
+        float* pixelData);
+    void shadeAlphaPixel(
+        int circleIndex,
+        float pixelCenterX, float pixelCenterY,
+        float px, float py, float pz,
+        float* pixelData);
+    void shadePixel(
+        int circleIndex,
+        float pixelCenterX, float pixelCenterY,
+        float px, float py, float pz,
+        float* pixelData);
+    void circleInBoxTest(int numCircles, int numBlocks, int blocksPerRow);
+
+};
+
+
+#endif
